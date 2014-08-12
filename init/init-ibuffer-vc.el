ca32e6aa04c66a53ecf0033aadca5da3f2d72b8c
Jonathon Moore <jon@webdigia.com>
2014-08-12 15:46:14 -0400

    iBuffer has most recent buffer selected

--- init/init-ibuffer-vc.el
+++ init/init-ibuffer-vc.el
@@ -15,3 +15,11 @@
                                  "\\*Buffer List\\*"
                                  "\\*Help\\*"
                                  "\\*Packages\\*"))
+
+;; Switching to ibuffer puts the cursor on the most recent buffer
+(defadvice ibuffer (around ibuffer-point-to-most-recent) ()
+  "Open ibuffer with cursor pointed to most recent buffer name"
+  (let ((recent-buffer-name (buffer-name)))
+    ad-do-it
+    (ibuffer-jump-to-buffer recent-buffer-name)))
+(ad-activate 'ibuffer)
