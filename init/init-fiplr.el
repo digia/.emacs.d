0691001201d66173f1e88268a0705848e0cdc997
digia <jon@digia.io>
2026-04-14 16:18:11 -0400

    chore: blank canvas for 2026 refactor

--- init/init-fiplr.el
+++
@@ -1,2 +0,0 @@
-(setq fiplr-ignored-globs '((directories (".git" ".svn" "bower_components" "node_modules" ".tmp" "tmp" "public/images" "public/img" "vendor"))
-                            (files ("*.jpg" "*.png" "*.zip" "*.elc" "*~"))))
